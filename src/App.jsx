471bc9e648122bfe46e7f4dfd3286d8776ce8a23
Umair <uak1911@gmail.com>
2024-07-12 02:18:47 +0530

    created the basic layout of the todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react"
 
-
+// npx tailwindcss -i ./src/index.css -o ./output.css --watch
 function App() {
 
+  const [todos, setTodos] = useState(["asldkfj","alskdjf"]);
   return (
-   <div>
-   <h1 className="underline">Things to do</h1>
+   <div className="grid place-content-center h-screen bg-slate-200">
+   <div className="bg-white p-4">
+      <div><h1 className="text-3xl font-bold">Things To Do</h1></div>
+      <div>
+      <input className="border-spacing-1 border-black" type="text"></input>
+
+      {todos.map((todo)=>{return(
+        <div key={todo} className="m-1 p-1">
+        <hr />
+        <input type="checkbox" /> <p className="inline">{todo}</p>
+        </div>
+        )
+      })}
+      </div>
+      </div>
+      <div className="bg-green-200">
+      {todos.length} items left
+      </div>
    </div>
   )
 }
