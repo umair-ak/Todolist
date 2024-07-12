e4ff14c1edc66121200beeada5838046e1d16b83
Umair <uak1911@gmail.com>
2024-07-12 23:37:10 +0530

    added the functionality of a todolist

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,37 @@ import { useState } from "react"
 // npx tailwindcss -i ./src/index.css -o ./output.css --watch
 
 function ButtonGroup() {
-  const [selectedButton, setSelectedButton] = useState(null);
+  const [selectedButton, setSelectedButton] = useState("button1");
 
   const handleButtonClick = (buttonId) => {
     setSelectedButton(buttonId);
   };
 
   return (
-    <div className="flex space-x-2">
+    <div className="flex justify-end space-x-2">
       <button
-        className={`px-1 rounded ${
+        className={`ml-auto px-1 rounded ${
           selectedButton === 'button1' ? 'border border-black' : ''
         }`}
         onClick={() => handleButtonClick('button1')}
       >
-        Button 1
+      All
       </button>
       <button
-        className={`px-1 rounded ${
+        className={`ml-auto px-1 rounded ${
           selectedButton === 'button2' ? 'border border-black' : ''
         }`}
         onClick={() => handleButtonClick('button2')}
       >
-        Button 2
+        Active
       </button>
       <button
-        className={`px-1 rounded ${
+        className={`ml-auto px-1 rounded ${
           selectedButton === 'button3' ? 'border border-black' : ''
-        }`}
-        onClick={() => handleButtonClick('button3')}
+          }`}
+          onClick={() => handleButtonClick('button3')}
       >
-        Button 3
+      Completed
       </button>
     </div>
   );
@@ -42,35 +42,50 @@ function ButtonGroup() {
 
 function App() {
 
-  const [todos, setTodos] = useState(["asldkfj","alskdjf","asldkfj","alskdjf","asldkfj","alskdjf","asldkfj"]);
+  const [todos, setTodos] = useState([]);
 
   const [completed, setCompleted] = useState([]);
+  
 
-  const [filter, setFilter] = useState('all'); // Default filter is 'all'
+  const handleRemoveTask = (task) => {
+    const updatedTodos = todos.filter((todo) => todo !== task);
+    setCompleted(...completed,task);
+    setTodos(updatedTodos);
+  };
 
-  // Filtered tasks based on the selected filter
-  const filteredTodos = todos.filter((todo) => {
-    if (filter === 'active') {
-      return !todo.completed;
-    } else if (filter === 'completed') {
-      return todo.completed;
+  const [newTask, setNewTask] = useState(''); 
+  
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      if (newTask.trim()) {
+        setTodos([...todos,newTask]);
+        setNewTask('');
+      }
     }
-    return true; // Show all tasks for 'all' filter
-  });
-
-
+  };
   return (
   <>
   <div className="mx-auto w-2/6 p-4 mt-10 bg-white border">
   <div className="text-3xl font-bold text-center m-3">Things To Do</div>
   
-      <input placeholder="Add new" className="w-full border focus:border-blue-300 p-1" type="text"></input>
+  <input
+    placeholder="Add new task"
+    className="w-full border focus:border-blue-300 p-1"
+    type="text"
+    value={newTask}
+    onChange={(e) => setNewTask(e.target.value)} 
+    onKeyPress={handleKeyPress}
+  />
+
+
       
-    <div className="max-h-3/5 overflow-y-auto">
-    {todos.map((todo)=>{return(
+    <div className="max-h-4/6 overflow-y-auto">
+    {todos && todos.map((todo)=>{return(
       <div className="" key={todo}>
       <hr className="my-2 border-gray-300"/>
-      <input type="checkbox" className="mr-2" />
+      <input type="checkbox" className="mr-2" 
+      onChange={() => handleRemoveTask(todo)}
+      />
       <p className="inline">{todo}</p>
       </div>
       )
@@ -78,7 +93,7 @@ function App() {
       </div>
       
     </div>
-    <div className="mx-auto w-2/6 p-3 bg-green-200 border flex text-xs">
+    <div className="mx-auto w-2/6 p-3 bg-green-200 border flex text-xs relative">
         
     <div>
     search button
@@ -88,7 +103,7 @@ function App() {
         {todos.length} items left
         </div>
         
-       <div className="float-end">
+       <div className="absolute right-1">
        <ButtonGroup />
        </div> 
 
